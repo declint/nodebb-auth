91c79857c5f3b57ea21be2638e471af1f1d27f94
Mattias <declint@LAPTOP-TFUV467C.localdomain>
2022-10-06 19:52:58 +0200

    Fungerande server

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,30 @@
 import React, {useEffect, useState} from 'react'
 
 
-function 
-App () {
+function App () {
 
   const [backendData, setBackendData] = useState([{}])
 
   useEffect(() => {
     fetch("/api").then(
-      response => response.json()
+      response => {
+        console.log(response);
+        return response.json()
+      }
+
     ).then(
       data => {
         setBackendData(data)
       }
     )
+
+    console.log("Testa lite output")
   }, [])
+
+  useEffect(() => {
+    console.log("Min andra useEffect");
+    
+  })
 
   return (
     <div>
--- server/server.js
+++ server/server.js
@@ -1,8 +1,45 @@
-const express = require('express')
-const app = express()
+const fetch = require('node-fetch');
 
-app.get("/api", (req, res) => {
-    res.json({"users":["user1", "user2", "Mattias"]})
-})
+let login_data = {
+    username: "declint",
+    password: "nada123"
+};
 
-app.listen(5000, () => {console.log("Server startad port 5000")})
+async function fetchCookie()
+{
+    const res = await fetch('http://localhost:4567/api/v3/utilities/login', {
+        method: 'POST',
+        body: JSON.stringify(login_data),
+        headers: { 'Content-Type': 'application/json' }});
+    const data = await res.json();
+
+    return res.headers.raw()['set-cookie'];
+}
+
+async function do_api_request(cookie_to_send){
+    const url = 'http://localhost:4567/api'
+    const opts = {
+        headers: {
+            cookie: cookie_to_send
+        }
+    };
+    const res = await fetch(url, opts);
+    const data = await res.json();//assuming data is json
+
+    return data
+}
+
+async function getCookieAndTheData()
+{
+    const superkaka = await fetchCookie();
+    console.log("Superkaka:");
+    console.log(superkaka);
+
+    const apireq = await do_api_request(superkaka);
+    console.log("API Req:");
+    console.log(apireq);
+}
+
+getCookieAndTheData();
+
+
